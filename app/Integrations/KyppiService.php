22e3e403991424ef51c47235ef652d49d0b8fc81
pnurmine <17642400+nurmp@users.noreply.github.com>
2020-11-25 14:27:14 +0200

    Logging changed

--- app/Integrations/KyppiService.php
+++ app/Integrations/KyppiService.php
@@ -167,7 +167,7 @@ class KyppiService{
         // <muinaisjaannos> elementin alta löytyy kaikki tarvittava
         $muinaisjaannos = $dom->getElementsByTagName( 'muinaisjaannos' )->item(0);
 
-        Log::debug($muinaisjaannos->saveXML());
+        Log::debug($dom->saveXML());
 
         // Kohteen luonti ja tietojen populointi
         $this->muodostaKohdeKypinTiedoilla($muinaisjaannos, $kohde);
