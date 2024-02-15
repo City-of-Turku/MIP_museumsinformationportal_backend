c44a78f0461efcc50e667f4b3eb1d8bcb3d56db4
Miisa Jalonen <miisa.jalonen@atrsoft.com>
2024-02-15 13:30:43 +0200

    Aiheiden ja muistojen tallennus

---
+++ app/Muistot/Muistot_kuva.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Muistot;
+
+use App\Library\Gis\MipGis;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+
+class Muistot_kuva extends Model {
+
+    protected $table = "muistot_kuva";
+    protected $primaryKey = 'prikka_id';
+    public $timestamps = false;
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'prikka_id',
+        'muistot_muisto_id',
+        'ccby',
+        'ehdot',
+        'polku',
+        'nimi',
+        'kuvaus',
+        'ottohetki',
+        'ottopaikka',
+        'valokuvaaja'
+    ];
+
+    public function muisto() {
+        return $this->belongsTo('App\Muistot\Muistot_muisto');
+    }
+
+    /**
+     * Get all Models from DB - order by given $order_field to given $order_direction
+     *
+     * @version 1.0
+     * @since 1.0
+     */
+    public static function getAll() {
+
+        $qry = Muistot_kuva::select('muistot_kuva.*')
+            ->leftJoin('muistot_muisto', 'muistot_muisto.prikka_id', '=', 'muistot_kuva.muistot_muisto_id');
+
+
+		return $qry;
+    }
+
+    /**
+     * Method to get all related to one muisto
+     *
+     * @param int $id
+     * @version 1.0
+     * @since 1.0
+     */
+    public static function get($id) {
+        return Muistot_kuva::select('muistot_kuva.*')
+            ->where('muistot_kuva.muistot_muisto_id', '=', $id);
+    }
+}
---
+++ app/Muistot/Muistot_kysymys.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Muistot;
+
+use App\Library\Gis\MipGis;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+
+class muistot_kysymys extends Model {
+
+    protected $table = "muistot_kysymys";
+    protected $primaryKey = 'prikka_id';
+    public $incrementing = false;
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'prikka_id',
+        'muistot_aihe_id',
+        'teksti_fi',
+        'teksti_en',
+        'teksti_sv'
+
+    ];
+
+    public function aihe()
+    {
+        $this->belongsTo('App\Muistot\Muistot_aihe');
+    }
+
+    public function vastaukset()
+    {
+        $this->hasMany('App\Muistot\Muistot_vastaus');
+    }
+
+    /**
+     * Get all Models from DB - order by given $order_field to given $order_direction
+     *
+     * @version 1.0
+     * @since 1.0
+     */
+    public static function getAll() {
+
+        $qry = muistot_kysymys::select('muistot_kysymys.*')
+            ->leftJoin('muistot_aihe', 'muistot_aihe.prikka_id', '=', 'muistot_kysymys.muistot_aihe_id');
+
+		return $qry;
+    }
+
+    /**
+     * Method to get single entity from db with given ID
+     *
+     * @param int $id
+     * @version 1.0
+     * @since 1.0
+     */
+    public static function getSingle($id) {
+        return muistot_kysymys::select('muistot_kysymys.*')
+            ->where('muistot_kysymys.prikka_id', '=', $id);
+    }
+}
