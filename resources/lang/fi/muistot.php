4ed66d8211cb661c6bc644180a8ef22f0ddfeeb8
Miisa Jalonen <miisa.jalonen@atrsoft.com>
2024-05-03 09:54:01 +0300

    Memories: Fix for date search and messages

--- resources/lang/fi/muistot.php
+++ resources/lang/fi/muistot.php
@@ -4,9 +4,9 @@ return [
 	'search_failed'			=> 'Muistojen haku epäonnistui.',
 	'search_not_found'		=> 'Muistoa ei löydy.',
 	'found_count'			=> ':count löytyi annetuilla hakuehdoilla.',
-	'save_failed'			=> 'Muistojen tietojen tallennus epäonnistui',
-	'save_success'			=> 'Muistojen tietojen tallennus onnistui.',
-	'delete_success'		=> 'Muistojen poisto onnistui.',
-	'delete_failed'			=> 'Muistojen poisto epäonnistui.',
-	'too_many_found'		=> 'Useita muistoja löytyi. Useilla muistoilla on sama tunnus.'
+	'save_failed'			=> 'Muiston tietojen tallennus epäonnistui',
+	'save_success'			=> 'Muiston tietojen tallennus onnistui.',
+	'delete_success'		=> 'Muiston poisto onnistui.',
+	'delete_failed'			=> 'Muiston poisto epäonnistui.',
+	'too_many_found'		=> 'Useita muistoja löytyi.'
 ];
---
+++ resources/lang/fi/muistot_aihe.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'search_success'		=> 'Aiheiden haku onnistui.',
+	'search_failed'			=> 'Aiheiden haku epäonnistui.',
+	'search_not_found'		=> 'Aihetta ei löydy.',
+	'found_count'			=> ':count löytyi annetuilla hakuehdoilla.',
+	'save_failed'			=> 'Aiheen tietojen tallennus epäonnistui',
+	'save_success'			=> 'Aiheen tietojen tallennus onnistui.',
+	'delete_success'		=> 'Aiheen poisto onnistui.',
+	'delete_failed'			=> 'Aiheen poisto epäonnistui.',
+	'too_many_found'		=> 'Useita aiheita löytyi.'
+];
